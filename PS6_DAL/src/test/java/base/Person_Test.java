9a8a50fc5b4eec5f4a11bff214a4b9576099897d
jared <jared@LAPTOP-SR55OSKM>
2017-05-06 20:09:05 -0400

    removed old repository. named this one the same. pushed all updated code

--- PS6_DAL/src/test/java/base/Person_Test.java
+++ PS6_DAL/src/test/java/base/Person_Test.java
@@ -2,20 +2,31 @@ package base;
 
 import static org.junit.Assert.*;
 
-import java.util.Date;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.UUID;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 
 import domain.PersonDomainModel;
 
 public class Person_Test {
+	
+	private PersonDomainModel test = new PersonDomainModel();
+	private UUID PersonID = UUID.randomUUID(); 
+	private boolean perDeleted= false; //needed to see if the delete method was run, then we dont attempt to delete the person after
 
+	
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		
 	}
 
 	@AfterClass
@@ -24,25 +35,54 @@ public class Person_Test {
 
 	@Before
 	public void setUp() throws Exception {
+		//add attributes to the one person
+		
+		test.setPersonID(PersonID);
+		test.setFirstName("Jared");
+		test.setLastName("Glaser");
+		test.setStreet("test");
+		test.setPostalCode(19717);
+		test.setCity("Newark");
+		test.setBirthday(new Date(0));
+		
+		PersonDAL.addPerson(test);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		if(!perDeleted)
+			PersonDAL.deletePerson(PersonID);
 	}
 
 	@Test
-	public void AddPerson() {
-		PersonDomainModel per1 = new PersonDomainModel();
-		per1.setBirthday(new Date(0));
-		per1.setCity("Townsend");
-		per1.setFirstName("Bert");
-		per1.setLastName("Gibbons");
-		per1.setPostalCode(19734);
-		per1.setStreet("214 Labrador Lane");
+	public void testAddAndGetPerson() { //uses the get person method to test if the add was done correctly in the before class
+		//get the person we added before the tests
+		PersonDomainModel gotPer = PersonDAL.getPerson(PersonID);
 		
-		PersonDAL.addPerson(per1);
+		
+		
+		assertTrue(gotPer.getPersonID().equals(test.getPersonID())); //see if we added the person correctly in the before class
 		
 		
 	}
+	
+	@Test
+	public void testDeletePerson(){
+		int initSize = PersonDAL.getPersons().size();
+		PersonDAL.deletePerson(PersonID); //deletes the person we added before the tests
+		int finalSize = PersonDAL.getPersons().size();
+		perDeleted = true; //the after method does not try to delete the person since we already deleted them
+		assertTrue((initSize - 1) == finalSize); //if there is one less person, we deleted them.
+	}
+	
+	@Test
+	public void testUpdatePerson(){
+		PersonDomainModel tempPer = PersonDAL.getPerson(PersonID);//grab the person we created at the beginning
+		tempPer.setCity("NewCity"); //change the city
+		PersonDAL.updatePerson(tempPer); //update the city of the person in the database
+		PersonDomainModel upPer = PersonDAL.getPerson(PersonID); //grab the person from the database
+		assertTrue(upPer.getCity().equals(tempPer.getCity()));//see if the new city matches the updated person in the database
+	}
+	
 
 }
